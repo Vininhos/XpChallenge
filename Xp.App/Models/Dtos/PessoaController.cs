3a189cb677a224ef8e86352b512b00f17c4bf9ff
Vininhos <viniciusrw2011@hotmail.com>
2023-02-14 23:18:07 -0300

    Criado PessoaController, PessoaDto, Index.cshtml e ajustado o header compartilhado

---
+++ Xp.App/Models/Dtos/PessoaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xp.App.Models.Dtos
+{
+    public class PessoaController : Controller
+    {
+        private static List<PessoaDto> _pessoas;
+
+        public PessoaController()
+        {
+            _pessoas = new List<PessoaDto>
+            {
+                new PessoaDto()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Nome = "Josnei F.",
+                    Email = "teste@teste.com"
+                },
+                new PessoaDto()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Nome= "Ronaldo S.",
+                    Email = "teste2@teste.com"
+                }
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListAll()
+        {
+            return View(_pessoas);
+        }
+    }
+}
---
+++ Xp.App/Models/Dtos/PessoaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Xp.App.Models.Dtos
+{
+    public class PessoaDto
+    {
+        public string Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
