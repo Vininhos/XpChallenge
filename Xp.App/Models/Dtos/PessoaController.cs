8dcf9111c6ffbbd84a2731878798f27b1b551bac
Vininhos <viniciusrw2011@hotmail.com>
2023-02-15 23:22:11 -0300

    Desenvolvido cadastro de pessoas

--- Xp.App/Models/Dtos/PessoaController.cs
+++ Xp.App/Models/Dtos/PessoaController.cs
@@ -8,6 +8,12 @@ namespace Xp.App.Models.Dtos
 
         public PessoaController()
         {
+            if (_pessoas == null)
+                LoadConfig();
+        }
+
+        private void LoadConfig()
+        {
             _pessoas = new List<PessoaDto>
             {
                 new PessoaDto()
@@ -33,6 +39,27 @@ namespace Xp.App.Models.Dtos
         public IActionResult ListAll()
         {
             return View(_pessoas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Nome, Email")] PessoaDto pessoa)
+        {
+            try
+            {
+                pessoa.Id = Guid.NewGuid().ToString();
+                _pessoas.Add(pessoa);
+
+                return RedirectToAction("ListAll");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
